6b3c726bf1a3fadcc4b009843cc487b2d38f29f0
alastair <ajwhirst@gmail.com>
2022-12-07 12:26:53 +0100

    new tagging approach using actual pytoml semver

--- tag.sh
+++ tag.sh
@@ -1,2 +1,6 @@
 set -ex
-git tag -fa v1.4.3 -m "my version 1.4.3"
+git fetch --all --tags
+git --no-pager tag
+export tag_name=2.4.1
+git tag -a v$tag_name -m "version $tag_name"
+git push origin v$tag_name
